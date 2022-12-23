d5a2391fed67812ccbccdccc793d87fb53f69b42
Jérémy Virant <jeremy@virant.fr>
2022-12-23 19:23:43 +0100

    2022: start day 6

---
+++ 2022/day_06/src/main.rs
@@ -0,0 +1,15 @@
+const INPUT_EXAMPLE: &str = "";
+const INPUT_PART1: &str = "";
+
+fn part1() {
+}
+
+fn part2() {
+}
+
+fn main() {
+    println!("part1");
+    part1();
+    println!("part2");
+    part2();
+}
